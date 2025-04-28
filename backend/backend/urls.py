b58d3625e815c74e0b0e3c20e16f93829579dfc4
Tudor Popescu <tudor.m.p@outlook.com>
2025-04-28 19:40:30 +0300

    fix: handle w3c checker issues

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from api.views import MyTokenObtainPairView
 # Pre-built views for JWT authentication
 from rest_framework_simplejwt.views import TokenRefreshView
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/user/register/', UserCreateView.as_view(), name='register'),
     path('api/token/', MyTokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     # Include the default authentication URLs provided by DRF
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    # Message for backend root directory
+    path('', lambda request: JsonResponse({'message': 'Django backend running ✅'})),
 ]
